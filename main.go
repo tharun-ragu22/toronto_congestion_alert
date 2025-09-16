684c7e78e2ba43f3324d00cca6352d665b5be33e
Tharun Ragulan <t2ragula@uwaterloo.ca>
2025-09-16 14:05:36 -0400

    can select expected and actual speeds from api response

--- main.go
+++ main.go
@@ -5,9 +5,20 @@ import (
 	"net/http"
 	"io"
 	"os"
+	"time"
+	"encoding/json"
 )
 
-func main() {
+type ApiResponse struct {
+	SpeedInformation SpeedInformation `json:"flowSegmentData"`
+}
+
+type SpeedInformation struct {
+	CurrentSpeed float64 `json:"currentSpeed"`
+	FreeFlowSpeed float64 `json:"freeFlowSpeed"`
+}
+
+func fetchAPIBody() {
 	resp, err := http.Get(fmt.Sprintf("https://api.tomtom.com/traffic/services/4/flowSegmentData/relative0/10/json?point=43.729889%%2C-79.447357&unit=KMPH&openLr=false&key=%s", os.Getenv("TOMTOM_API_KEY")))
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -21,5 +32,30 @@ func main() {
     	fmt.Printf("Error reading response body: %v\n", err)
     	return
     }
-    fmt.Printf("Response body: %s\n", body)
+    // fmt.Printf("Response body: %s\n", body)
+
+	var apiResponse ApiResponse
+	err = json.Unmarshal(body, &apiResponse)
+	if (err != nil) {
+		fmt.Printf("Error unmarshalling JSON: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Current Speed: %f, Free Flow Speed: %f\n", apiResponse.SpeedInformation.CurrentSpeed, apiResponse.SpeedInformation.FreeFlowSpeed)
+
+
+}
+
+func main() {
+	ticket := time.NewTicker(5 * time.Second)
+	defer ticket.Stop()
+	fetchAPIBody()
+
+	for {
+		select {
+		case <-ticket.C:
+			fmt.Printf("Ticker ticked at %v\n", time.Now())
+			fetchAPIBody()
+		}
+	}
 }
